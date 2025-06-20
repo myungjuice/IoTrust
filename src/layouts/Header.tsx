3822a1bf9c5f82a382e1dce38578e7a0b1046456
myungjuice <wkdaudwn1028@gmail.com>
2025-06-20 15:34:28 +0900

    fix: header tag 수정

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,12 +2,12 @@ import Image from '@/components/ui/Image';
 
 export default function Header() {
   return (
-    <div className='flex h-16 items-center justify-start px-2'>
+    <header className='flex h-16 items-center justify-start px-2'>
       <Image
         src='https://assets.moyoplan.com/logo/moyo-icon.svg'
         alt='logo'
         className='h-10 w-14 px-2'
       />
-    </div>
+    </header>
   );
 }
